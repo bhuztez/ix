17f0ca70b61a8fd8833d430b3d1f639dd708cfb1
bhuztez <bhuztez@gmail.com>
2017-12-30 11:21:46 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name = 'ix',
+    version = '0.0',
+
+    url = 'https://github.com/bhuztez/ix',
+    description = ' command line online judge client',
+    license = 'BSD',
+
+    classifiers = [
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+
+    author = 'bhuztez',
+    author_email = 'bhuztez@gmail.com',
+
+    packages = ['ix', 'ix.clients', 'ix.credential', 'ix.credential.readers', 'ix.credential.storages'],
+    install_requires= ['lxml', 'websocket-client'],
+    zip_safe = False,
+)
